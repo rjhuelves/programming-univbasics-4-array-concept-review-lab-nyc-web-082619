b831b1e24c0f045045ea1994d9fb42ce5f12bc10
Richard Huelves <rhuelves@gmail.com>
2019-08-19 20:34:59 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,11 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array = []
+  
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array = [[2,3,4,5], [6,5,4,3], [1,2,3,4,5,4,3,2,1] [5,4,3,2,1,0,1,2,3,4,5], [500, 4, 1000, 5, 250]]
+  array.flatten.max
 end
 
 def find_min_value(array)
