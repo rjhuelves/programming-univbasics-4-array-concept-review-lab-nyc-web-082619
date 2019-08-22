f7ca4e8bf12c4428ec5da1608fb720ad8cfc6151
Richard Huelves <rhuelves@gmail.com>
2019-08-22 01:55:19 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,32 @@
 def find_element_index(array, value_to_find)
-  array = []
-  
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+    end
+  end
+  nil
 end
 
 def find_max_value(array)
-  array = [[2,3,4,5], [6,5,4,3], [1,2,3,4,5,4,3,2,1] [5,4,3,2,1,0,1,2,3,4,5], [500, 4, 1000, 5, 250]]
-  array.flatten.max
+  max = 0
+    array.length.times do |count|
+      if array[count] > max
+        max = array[count]
+      end
+    end
+  max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  min = 0
+    array.length.times do |count|
+      if count == 0
+        min = array[count]
+      else
+        if array[count] < min
+          min = array[count]
+        end
+      end
+    end
+  min
 end
